eef80cde3a70a8f4edfd3a1b748b6c4aa088b07e
Vallis <me@work>
2025-08-25 20:24:55 +0100

    add perms script

--- src/instant.perms.ts
+++ src/instant.perms.ts
@@ -1,27 +1,58 @@
+// // Docs: https://www.instantdb.com/docs/permissions
+
+// import type { InstantRules } from "@instantdb/react";
+
+// // docs https://www.instantdb.com/docs/permissions
+
+// const standardRules = {
+//     allow: {
+//       view: "isOwner",
+//       create: "isOwner",
+//       update: "isOwner && isStillOwner",
+//       delete: "false",
+//     },
+//     bind: [
+//       "isOwner", "auth.id != null && auth.id == data.creatorId",
+//       "isStillOwner", "auth.id != null && auth.id == newData.creatorId"
+//     ]
+//   }
+
+// const rules = {
+//   item: standardRules,
+//   shopListItem: standardRules,
+//   template: standardRules,
+//   templateItem: standardRules,
+//   attrs: {
+//     allow: {
+//       $default: 'false',
+//     },
+//   },
+// } satisfies InstantRules;
+
+// export default rules;
+
+
 // Docs: https://www.instantdb.com/docs/permissions
 
 import type { InstantRules } from "@instantdb/react";
 
-// docs https://www.instantdb.com/docs/permissions
-
-const standardRules = {
-    allow: {
-      view: "isOwner",
-      create: "isOwner",
-      update: "isOwner && isStillOwner",
-      delete: "false",
-    },
-    bind: [
-      "isOwner", "auth.id != null && auth.id == data.creatorId",
-      "isStillOwner", "auth.id != null && auth.id == newData.creatorId"
-    ]
-  }
-
 const rules = {
-  item: standardRules,
-  shopListItem: standardRules,
-  template: standardRules,
-  templateItem: standardRules,
+  /**
+   * Welcome to Instant's permission system!
+   * Right now your rules are empty. To start filling them in, check out the docs:
+   * https://www.instantdb.com/docs/permissions
+   *
+   * Here's an example to give you a feel:
+   * posts: {
+   *   allow: {
+   *     view: "true",
+   *     create: "isOwner",
+   *     update: "isOwner",
+   *     delete: "isOwner",
+   *   },
+   *   bind: ["isOwner", "auth.id != null && auth.id == data.ownerId"],
+   * },
+   */
   attrs: {
     allow: {
       $default: 'false',
