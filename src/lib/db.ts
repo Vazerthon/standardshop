809daa2e095e45c3affc0fba314af8813007e400
Vallis <me@work>
2025-08-27 20:56:10 +0100

    Bake instant app id in

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { init, id, lookup  } from '@instantdb/react';
 import schema from '@/instant.schema';
 
-const APP_ID = import.meta.env.VITE_INSTANT_APP_ID;
+const APP_ID = "fe566e87-9a00-421e-b582-747a4fa7fa91";
 
 export const db = init({ appId: APP_ID, schema });
 
